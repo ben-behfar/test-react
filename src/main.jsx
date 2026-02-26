28040073f980989b7193f1b55b900241cad02b2f
ben <benbehfar7@gmail.com>
2026-02-26 19:34:22 +0330

    adding component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,41 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<h1>hi</h1>);
+class Timer extends React.Component{
+render(){
+  return(
+      <h2>
+         the time is {new Date().toLocaleTimeString()}
+      </h2>
+    )
+  }
+}
+
+class Polite extends React.Component{
+  render(){
+    return(
+        <div>
+          <h1>hello there</h1>
+        </div>
+    )
+  }
+}
+
+class Appx extends React.Component {
+  render(){
+    return(
+      <div>
+        <Polite/>
+        <Timer/>
+      </div>
+    )
+  }
+}
+
+const prog = () =>{
+
+  createRoot(document.getElementById('root')).render(<Appx/>);
+}
+  setInterval(() => {prog()} ,1000)
